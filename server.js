2e2084a1a6d2f319c82e04793656f9ed6c8115a1
Jerry Sawyer <jerrymsawyer@yahoo.com>
2025-08-14 08:59:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// This serves all files in your folder (like CSS or images in the future)
 app.use(express.static(__dirname));
 
-app.get('/*', (req, res) => {
+// This specifically handles the main page request
+app.get('/', (req, res) => {
+  // We explicitly tell the browser this is an HTML file
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
